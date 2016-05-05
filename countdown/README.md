fbd43283befe2d021c6000c5e4bc8701f44cf1fc
YaoBin <yaob421123@163.com>
2016-05-05 15:04:15 +0800

    jquery倒计时插件

---
+++ countdown/README.md
@@ -0,0 +1,26 @@
+﻿# 使用说明
+
+用jquery 封装的一个倒计时插件。
+
+## 使用方法 :
+
+```bash
+<div class="count">点击我倒计时<i></i></div>
+```
+
+## js :
+
+导入 `jquery` 和 `countdown.js`
+
+```bash
+$(function(){
+	$('.count').CountDown(num, callback);
+})
+```
+
+## 参数说明
+
+- `num` 可选。倒计时时间(秒)
+
+- `callback`  可选。事件执行后要执行的函数。
+
---
+++ countdown/countdown.js
@@ -0,0 +1,53 @@
+!(function($){
+	$.fn.CountDown = function(num, callback){
+		return this.each(function() {
+            var _this = $(this);
+            //单例模式
+            var date = _this.data('countDown');
+            if(!date){
+                date = new countDown(_this, num, callback);
+                _this.data('countDown', date);
+            }
+        });
+	}
+	var countDown = function(elments,num, callback){
+		this.elments = elments;
+        if(typeof num == 'string'){
+            this.num = parseInt(num);
+        }else if(typeof num == 'function'){
+            this.num = 120;
+            this.callback = num;
+        }else{
+            this.num = num || 120;
+        }
+        this.callback = callback;
+        console.log(this.num)
+
+        this.timer = null;
+        this.init();
+    }
+    countDown.prototype = {
+    	init : function(){
+    		this.change();
+    	},
+        change : function(){
+            var _this = this;
+            this.elments.on('click', function() {
+                var num = _this.num;
+                clearInterval(_this.timer);
+                _this.elments.css('pointer-events', 'none')
+                var $html = _this.elments.html();
+                _this.timer = setInterval(function(){
+                    num --;
+                    _this.elments.find('i').html('(' +num+ ')'); 
+                    if(num <= 0){
+                        clearInterval(_this.timer);
+                        _this.elments.find('i').html('');
+                        _this.elments.css('pointer-events', 'auto')
+                    }
+                }, 1000);
+                if(_this.callback){}
+            });
+        }
+    }
+})(jQuery)
